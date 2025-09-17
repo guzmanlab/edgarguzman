15f61456537fd07dbf78db874bff97a3e36b8364
Edgar Guzman <gamingtop@yahoo.com>
2025-09-16 21:01:56 -0700

    feat: add next package to have a standard config file

---
+++ toolings/next/src/root.mjs
@@ -0,0 +1,16 @@
+const packages = [
+  'tailwind',
+  'typescript'
+];
+
+/** @type {import("next").NextConfig[]} * */
+export const nextJsConfig = {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  transpilePackages: packages.map(module => `@edgarguzman/${module}`)
+};
