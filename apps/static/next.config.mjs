68cb484342803151aee8e6b5b3232fa3ce921420
Edgar Guzman <gamingtop@yahoo.com>
2025-10-16 20:58:38 -0400

    fix(feat/add-static-app): rewrite configuration corrected

--- apps/static/next.config.mjs
+++ apps/static/next.config.mjs
@@ -1,7 +1,7 @@
 import { nextJsConfig } from '@edgarguzman/next';
-import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
+/** @type {import("next").NextConfig} */
+const nextConfig = {
     ...nextJsConfig
 };
 
