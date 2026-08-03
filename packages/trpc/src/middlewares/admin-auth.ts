1a13c8e87f8f318bafc484a7f86975ce9766683c
Edgar Guzman <gamingtop@yahoo.com>
2026-08-02 21:35:15 -0400

    refactor: improvement to trpc middleware for admin procedure

--- packages/trpc/src/middlewares/admin-auth.ts
+++ packages/trpc/src/middlewares/admin-auth.ts
@@ -1,6 +1,31 @@
 import { TRPCError } from "@trpc/server";
+import { eq } from "drizzle-orm";
 
+import { roleEnum, user } from "../../../drizzle/src/schema/user";
 import { middleware } from "../client";
-import { userAuthMiddleware } from "../middlewares/user-auth";
 
-export const adminAuthMiddleware = middleware(async () => {});
+export const adminAuthMiddleware = middleware(async ({ ctx, next }) => {
+    if (!ctx.session?.user)
+        throw new TRPCError({
+            code: "UNAUTHORIZED",
+            message: "not authenticated, only administrator can access this level of procedure protocol",
+        });
+
+    let [currentUser] = await ctx.drizzle
+        .select({
+            role: user.role
+        })
+        .from(user)
+        .where(eq(user.id, ctx.session.user.id))
+        .limit(1);
+
+    if (currentUser?.role !== roleEnum.enumValues[1])
+        throw new TRPCError({
+            code: "UNAUTHORIZED",
+            message: "not authenticated, only administrator can access this level of procedure protocol",
+        });
+
+    return next({
+        ctx
+    });
+});
