07c9c68bd94dbb2b73f3b514892481a17a31ee80
Edgar Guzman <gamingtop@yahoo.com>
2026-09-18 15:22:52 -0400

    feat(delivery-api): add trpc endpoint

---
+++ apps/delivery/src/trpc/proxy.ts
@@ -0,0 +1,14 @@
+import type { AppRouter } from "@edgarguzman/trpc";
+import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+
+export const trpc = createTRPCProxyClient<AppRouter>({
+    links: [
+        httpBatchLink({
+            url: `${fetchBaseUrl()}/api/trpc`,
+        }),
+    ],
+});
+
+function fetchBaseUrl() {
+    return "http://localhost:3004";
+}
