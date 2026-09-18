bc27add2e896f061fd00479d5d11c63c5775f176
Edgar Guzman <gamingtop@yahoo.com>
2026-09-18 15:22:52 -0400

    feat(delivery-api): add trpc endpoint

---
+++ apps/delivery/src/app/api/trpc/[trpc]/route.ts
@@ -0,0 +1,29 @@
+import { appRouter, createContext as createTRPCContext } from "@edgarguzman/trpc";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import type { NextRequest } from "next/server";
+
+async function createContext(request: NextRequest) {
+    return await createTRPCContext({
+        headers: request.headers,
+    });
+}
+
+async function handler(request: NextRequest) {
+    return await fetchRequestHandler({
+        endpoint: "/api/trpc",
+        req: request,
+        router: appRouter,
+        async createContext() {
+            return await createContext(request);
+        },
+        onError: process.env.NODE_ENV === "development"
+          ? ({ path, error }) => {
+            console.error(
+              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
+            );
+          }
+          : undefined,
+    });
+}
+
+export { handler as GET, handler as POST };
--- apps/delivery/src/app/page.tsx
+++ apps/delivery/src/app/page.tsx
@@ -1,19 +1,41 @@
 import { Button } from "@edgarguzman/ui/button";
 import type { Metadata, NextPage } from "next";
 
+import { trpc } from "@/trpc/proxy";
+
 export function metadata(): Metadata {
   return {
     title: "Edgar Guzman",
   };
 }
 
-const Home: NextPage = () => {
+async function displayAllOrders() {
+    return await trpc.order.all.query({});
+}
+
+async function displayAllProducts() {
+    return await trpc.product.all.query();
+}
+
+const Home: NextPage = async () => {
+  let orderlist = await displayAllOrders();
+
+  let productlist = await displayAllProducts();
+
   return (
     <main className="grid min-h-screen grid-cols-1 place-items-center text-black">
       <div className="grid grid-col-1 place-items-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-semibold tracking-tight sm:text-[5rem]">
           Coming Soon
         </h1>
+
+        <div className="">
+            {JSON.stringify(orderlist, null, 4)}
+        </div>
+
+        <div className="">
+            {JSON.stringify(productlist, null, 4)}
+        </div>
 
         <div className="grid grid-cols-1 grid-rows-2 gap-4 sm:grid-cols-1 sm:grid-rows-2 md:gap-8">
           <div className="text-lg">
